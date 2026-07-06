483054db700d4778e8289b7b6c544a031e7ab4ee
yiik223 <b.figueroa.baile@gmail.com>
2026-07-06 16:06:45 -0400

    Creacion de tablero: implementacion de CRUD

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+#Configuracion BD
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#base de datos, conexion con flask 
+db = SQLAlchemy(app)
+
+#estructura base de datos 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.String(20), default="porhacer")
+
+
+#POST es para subnir cambios y pos GET es para obtener infomracion, PUT es actualizar alguna tabla existente.
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+
+        new_task = Task(title=title, description=description)
+        db.session.add(new_task)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
+
+
+#eliminar una tarea en base al id de esta misma.
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+#editar la lista de las tareas en base a su id.
+@app.route("/edit/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+
+    if request.method == "POST":
+        task.title = request.form["title"]
+        task.description = request.form["description"]
+        task.status = request.form["status"]
+
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    return render_template("edit.html", task=task)
+
+#constructor 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
